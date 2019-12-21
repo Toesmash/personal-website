2cbf6e752e6fafc2ff5a92e79270ff78ca4cc11f
Tomas Chvalny <tomas@gameeapp.com>
2019-12-21 18:07:06 +0100

    Defined basic styles, global variables

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from 'gatsby';
-import Layout from '../components/Layout';
+import Layout from '../components/Layout/Layout';
+import '../styles/app.scss';
 
 const HomePage = () => (
   <Layout>
