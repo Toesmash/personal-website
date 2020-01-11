98c2d2a679249fc8fc735a3633f7cbaa236e732d
Tomas Chvalny <tomas@gameeapp.com>
2020-01-11 15:37:22 +0100

    Added centered Layout prop

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,29 +1,37 @@
-import React from "react";
+import React, { Fragment } from "react";
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 
 import NavBar from '../NavBar/NavBar';
 import styles from './Layout.module.scss';
 
-const Layout = ({ children, className }) => {
-  return (
-    <div className={cx(styles.wrapper, className)}>
-      <NavBar />
+const Layout = ({ children, isCentered, className }) => (
+  <Fragment>
+    <NavBar />
+    <div className={cx(
+      styles.wrapper,
+      {
+        [styles.centered]: isCentered,
+      },
+      className
+    )}>
       {children}
     </div>
-  );
-};
+  </Fragment>
+);
 
 Layout.propTypes = {
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node
   ]).isRequired,
+  isCentered: PropTypes.bool,
   className: PropTypes.string,
 };
 
 Layout.defaultProps = {
   className: '',
+  isCentered: false,
 };
 
 export default Layout;
